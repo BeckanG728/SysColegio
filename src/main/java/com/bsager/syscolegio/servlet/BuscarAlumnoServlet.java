39074b71d900fb25c5478c1c706250fdbf7dc147
BeckanG728 <209495631+BeckanG728@users.noreply.github.com>
2026-04-03 21:45:18 -0500

    C4

--- src/main/java/com/bsager/syscolegio/servlet/BuscarAlumnoServlet.java
+++ src/main/java/com/bsager/syscolegio/servlet/BuscarAlumnoServlet.java
@@ -4,8 +4,15 @@
  */
 package com.bsager.syscolegio.servlet;
 
+import com.bsager.syscolegio.cipher.CifradoCesar;
+import com.bsager.syscolegio.dto.request.AlumnoRequest;
+import com.bsager.syscolegio.dto.response.AlumnoResponse;
+import com.bsager.syscolegio.dto.response.ErrorResponse;
+import com.bsager.syscolegio.service.AlumnoService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -19,29 +26,44 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(name = "BuscarAlumnoServlet", urlPatterns = {"/alumno/buscar"})
 public class BuscarAlumnoServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    
+    private final AlumnoService service = new AlumnoService();
+    
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
+        response.setContentType("text/plain;charset=UTF-8");
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonResponse;
+        
         try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet BuscarAlumnoServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet BuscarAlumnoServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+            String bodyCifrado = new String(request.getInputStream().readAllBytes(), StandardCharsets.UTF_8);
+            
+            String bodyPlano = CifradoCesar.descifrar(bodyCifrado);
+            
+            AlumnoRequest object = mapper.readValue(bodyPlano, AlumnoRequest.class);
+            
+            AlumnoResponse resp = service.findByDNI(object.dni());
+            
+            if(resp == null || "error".equals(resp.resultado())){
+                response.setStatus(HttpServletResponse.SC_NOT_FOUND); // 401
+                String mensaje = "Alumno no encontrado";
+                ErrorResponse error = new ErrorResponse("error", mensaje);
+                jsonResponse = mapper.writeValueAsString(error);
+            } else {
+                response.setStatus(HttpServletResponse.SC_OK);
+                jsonResponse = mapper.writeValueAsString(resp);
+            }
+            
+            // Cifrar respuesta con César y envia al frontend
+            out.print(CifradoCesar.cifrar(jsonResponse));
+            
+        } catch (Exception e) {
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            try (PrintWriter out = response.getWriter()) {
+                ErrorResponse error = new ErrorResponse("error", "Error procesando la solicitud");
+                out.print(CifradoCesar.cifrar(mapper.writeValueAsString(error)));
+            }
+
         }
     }
 
